6960070e9a24ab495aeae846c3fa8bb3f14313b5
Ritik Jaiswal <ritikjaiswal148@gmail.com>
2023-01-04 23:43:54 +0530

    session2

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 // Created Nested React Elements
     const heading1 = React.createElement( "h1", {  },  "Namaste Everyone !!!");
-    const heading2 = React.createElement("h2", { }, "Namaste!!!");
-    const container = React.createElement("div", {  }, [heading1, heading2]);
+    const heading2 = React.createElement("h2", { }, "Parcel is a BEAST");
+    const container = React.createElement("div",  {id : "container" }, [heading1, heading2]);
 
 // Create Root using createroot
     const root = ReactDOM.createRoot(document.getElementById("root"));
