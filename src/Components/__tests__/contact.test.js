a4c7b4f6462eb7ff5fc11e575d271acf03f99a58
Ritik <ritikjaiswal148@gmail.com>
2024-01-16 10:44:06 +0530

    testing updates

--- src/Components/__tests__/contact.test.js
+++ src/Components/__tests__/contact.test.js
@@ -1,5 +1,25 @@
+import Contact from "../Contact";
+import {screen,render} from "@testing-library/react";
+import "@testing-library/jest-dom";
 
 
 test("should component is loaded", ()=>{
+render(<Contact/>);
+ const heading=screen.getByRole("heading");
+ expect(heading).toBeInTheDocument();
 
-})
+}); 
+
+test("should button is loaded", ()=>{
+    render(<Contact/>);
+     const button=screen.getByRole("button");
+     expect(button).toBeInTheDocument();
+    
+    }); 
+
+    test("should laod 2 input box ", ()=>{
+        render(<Contact/>);
+         const input=screen.getAllByRole("textbox"); 
+         expect(input.length).toBe(2);
+        
+        }); 
